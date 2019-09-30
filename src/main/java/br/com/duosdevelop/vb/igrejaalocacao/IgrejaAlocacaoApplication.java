3a901b4181853f86b6b8738d3f04b60ed1e6a64d
Gerson Junior <gersonjunior1771@gmail.com>
2019-09-30 11:55:53 -0300

    Inicio de projeto

---
+++ src/main/java/br/com/duosdevelop/vb/igrejaalocacao/IgrejaAlocacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.duosdevelop.vb.igrejaalocacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgrejaAlocacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgrejaAlocacaoApplication.class, args);
+	}
+
+}
