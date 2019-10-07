fc6c7fe7c064898ef3e80d08ef8e1f04ade54013
Gerson Junior <gersonjunior1771@gmail.com>
2019-10-07 16:21:36 -0300

    Criação de Repository

---
+++ src/main/java/br/com/duosdevelop/vb/igrejaalocacao/repositories/CelulaRepository.java
@@ -0,0 +1,9 @@
+package br.com.duosdevelop.vb.igrejaalocacao.repositories;
+
+import br.com.duosdevelop.vb.igrejaalocacao.domain.Celula;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CelulaRepository extends JpaRepository<Celula, Integer> {
+}
---
+++ src/main/java/br/com/duosdevelop/vb/igrejaalocacao/repositories/IgrejaRepository.java
@@ -0,0 +1,9 @@
+package br.com.duosdevelop.vb.igrejaalocacao.repositories;
+
+import br.com.duosdevelop.vb.igrejaalocacao.domain.Igreja;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IgrejaRepository extends JpaRepository<Igreja, Integer> {
+}
