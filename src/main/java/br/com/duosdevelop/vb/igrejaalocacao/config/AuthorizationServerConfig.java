7c15265c9f160697f600f5ff18a44118cfd4a3e6
Gerson Junior <gersonjunior1771@gmail.com>
2020-06-07 19:42:36 -0300

    Retirando refresh token da body e adionando aos cookies

--- src/main/java/br/com/duosdevelop/vb/igrejaalocacao/config/AuthorizationServerConfig.java
+++ src/main/java/br/com/duosdevelop/vb/igrejaalocacao/config/AuthorizationServerConfig.java
@@ -27,7 +27,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .secret("m0bil3client")
                 .scopes("read", "write")
                 .authorizedGrantTypes("password", "refresh_token")
-                .accessTokenValiditySeconds(1800)
+                .accessTokenValiditySeconds(20)
                 .refreshTokenValiditySeconds(3600 * 24);
     }
 
