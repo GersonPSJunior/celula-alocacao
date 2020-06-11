1c64b7e1b4cff4c5852795575b5d2a6cfe3eec3a
Gerson Junior <gersonjunior1771@gmail.com>
2020-06-10 21:07:09 -0300

    Cors configurado

--- src/main/java/br/com/duosdevelop/vb/igrejaalocacao/config/AuthorizationServerConfig.java
+++ src/main/java/br/com/duosdevelop/vb/igrejaalocacao/config/AuthorizationServerConfig.java
@@ -27,7 +27,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .secret("m0bil3client")
                 .scopes("read", "write")
                 .authorizedGrantTypes("password", "refresh_token")
-                .accessTokenValiditySeconds(20)
+                .accessTokenValiditySeconds(1800)
                 .refreshTokenValiditySeconds(3600 * 24);
     }
 
---
+++ src/main/java/br/com/duosdevelop/vb/igrejaalocacao/cors/CorsFilter.java
@@ -0,0 +1,36 @@
+package br.com.duosdevelop.vb.igrejaalocacao.cors;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+    private String originPermitida = "http://localhost:8000"; // TODO: Configurar para diferentes ambientes
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        resp.setHeader("Access-Control-Allow-Origin", originPermitida);
+        resp.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if ("OPTIONS".equals(req.getMethod()) && originPermitida.equals(req.getHeader("Origin"))) {
+            resp.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+            resp.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+            resp.setHeader("Access-Control-Max-Age", "3600");
+
+            resp.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+}
